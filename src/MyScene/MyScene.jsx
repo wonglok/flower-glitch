eaa9464dd571ebb544a52dd8dbe0eb8c2a025502
Wong Lok <wonglok@users.noreply.github.com>
2022-11-22 20:37:34 +0800

    save

--- src/MyScene/MyScene.jsx
+++ src/MyScene/MyScene.jsx
@@ -15,7 +15,6 @@ import {
   Mesh,
   MeshBasicMaterial,
   PlaneGeometry,
-  PerspectiveCamera,
   sRGBEncoding,
   WebGLRenderer,
   RepeatWrapping,
@@ -23,22 +22,24 @@ import {
   RGBAFormat,
   FloatType,
   Color,
-  TextureLoader,
   ShaderMaterial,
   Clock,
+  OrthographicCamera,
+  LinearEncoding,
 } from "three";
 import { FullScreenQuad } from "three/examples/jsm/postprocessing/EffectComposer";
-import { GLOverlay } from "../GLOverlay";
+// import { GLOverlay } from "../GLOverlay";
 
 export function MyScene() {
   let redBGTexture = useTexture(`/img/bg_red.jpg`);
   let flowerTexture = useTexture(`/test_png_for_loklok/base_flower001.png`);
   let linesTexture = useTexture(`/test_png_for_loklok/gen_line001.png`);
-  redBGTexture.encoding = sRGBEncoding;
-  flowerTexture.encoding = sRGBEncoding;
-  linesTexture.encoding = sRGBEncoding;
+  // redBGTexture.encoding = sRGBEncoding;
+  // flowerTexture.encoding = sRGBEncoding;
+  // linesTexture.encoding = sRGBEncoding;
 
   let texs = {
+    redBGTexture,
     flowerTexture,
     linesTexture,
   };
@@ -89,90 +90,154 @@ class VideoAPI {
       encoding: sRGBEncoding,
     });
 
-    this.uniforms = {
+    this.masterIntensity = { value: 1 };
+
+    this.record = async () => {
       //
+      // const HME = await import("h264-mp4-encoder").then((e) => e.default);
+    };
+
+    this.uniforms0 = {
+      masterIntensity: this.masterIntensity,
       time: {
         value: 0,
       },
-      lines: {
-        //
+      imageTexture: {
         value: null,
       },
-      flower: {
-        value: null,
-        //
-      },
-      //
     };
+
+    this.uniforms1 = {
+      masterIntensity: this.masterIntensity,
+      time: {
+        value: 0,
+      },
+      imageTexture: {
+        value: null,
+      },
+    };
+
+    this.uniforms2 = {
+      masterIntensity: this.masterIntensity,
+
+      time: {
+        value: 0,
+      },
+      imageTexture: {
+        value: null,
+      },
+    };
+
     let iClock = new Clock();
+
     let clock = {
       get delta() {
-        return iClock.getDelta();
+        return iClock.getElapsedTime();
       },
     };
 
     this.onLoop(() => {
-      this.uniforms.time.value = clock.delta;
-      this.uniforms.lines.value = texs.linesTexture;
-      this.uniforms.flower.value = texs.flowerTexture;
+      this.uniforms1.time.value = clock.delta;
+      this.uniforms1.imageTexture.value = texs.flowerTexture;
+
+      this.uniforms2.time.value = clock.delta;
+      this.uniforms2.imageTexture.value = texs.linesTexture;
     });
 
-    this.fsQuad = new FullScreenQuad(
-      new ShaderMaterial({
+    this.fScene = new Scene();
+
+    let makeShader = ({ uniforms }) => {
+      let shader = new ShaderMaterial({
         //
-        uniforms: this.uniforms,
+        transparent: true,
+        uniforms: uniforms,
         vertexShader: `
           varying vec2 vUv;
           void main () {
             vUv = uv;
             gl_Position = vec4(position, 1.0);
+            gl_Position.z += 0.01;
           }
         `,
         fragmentShader: /* glsl */ `
-        uniform sampler2D lines;
-        uniform sampler2D flower;
+        uniform sampler2D imageTexture;
         uniform float time;
+        uniform float masterIntensity;
         varying vec2 vUv;
 
-        #define RATE 0.0005
+        #define RATE 0.00025
 
         float rand(vec2 co){
           return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453) * 2.0 - 1.0;
         }
 
-        float offset(float blocks, vec2 uv) {
-          float shaderTime = time * RATE;
+        float offset(float blocks, vec2 uv, float rate) {
+          float shaderTime = time * rate;
           return rand(vec2(shaderTime, round(uv.y * blocks)));
         }
 
+        vec4 sRGBToLinear( in vec4 value ) {
+          return vec4( mix( pow( value.rgb * 0.9478672986 + vec3( 0.0521327014 ), vec3( 2.4 ) ), value.rgb * 0.0773993808, vec3( lessThanEqual( value.rgb, vec3( 0.04045 ) ) ) ), value.a );
+        }
+
         void main () {
-          float intensity = 20.1;
-          float moveAmount = 1.1;
+          float intensity = 1.0;
+          float moveAmount = 1.0;
+          float rate = 0.0;
 
-          vec2 uvNoiseR = moveAmount * vec2(offset(intensity, vUv) * 0.03, 0.01);
-          vec2 uvNoiseG = moveAmount * vec2(offset(intensity, vUv) * 0.03 * 0.1, 0.0);
-          vec2 uvNoiseB = moveAmount * vec2(offset(intensity, vUv) * 0.03, -0.01);
+          //
+          intensity = 8.1 * masterIntensity;
+          moveAmount = 2.0 * 0.01 * masterIntensity;
+          rate = 0.0001 * 0.42 * masterIntensity;
+          vec2 uv1NoiseR = moveAmount * vec2(offset(intensity, vUv, rate), 0.0);
+          vec2 uv1NoiseG = moveAmount * vec2(offset(intensity, vUv, rate), 0.0);
+          vec2 uv1NoiseB = moveAmount * vec2(offset(intensity, vUv, rate), 0.0);
+          vec2 uv1NoiseA = moveAmount * vec2(offset(intensity, vUv, rate), 0.0);
 
-          vec4 linesColor = texture2D(lines, vUv);
-          linesColor.r = texture2D(lines, vUv + uvNoiseR).r;
-          linesColor.g = texture2D(lines, vUv + uvNoiseG).g;
-          linesColor.b = texture2D(lines, vUv + uvNoiseB).b;
+          //
+          intensity = 250.1 * masterIntensity;;
+          moveAmount = 2.5 * 0.01 * masterIntensity;;
+          rate = 0.0001 * masterIntensity;;
+          vec2 uv2NoiseR = moveAmount * vec2(offset(intensity, vUv, rate), 0.0);
+          vec2 uv2NoiseG = moveAmount * vec2(offset(intensity, vUv, rate), 0.0);
+          vec2 uv2NoiseB = moveAmount * vec2(offset(intensity, vUv, rate), 0.0);
+          vec2 uv2NoiseA = moveAmount * vec2(offset(intensity, vUv, rate), 0.0);
 
-          vec4 flowerColor = texture2D(flower, vUv);
-          flowerColor.r = texture2D(flower, vUv + uvNoiseR).r;
-          flowerColor.g = texture2D(flower, vUv + uvNoiseG).g;
-          flowerColor.b = texture2D(flower, vUv + uvNoiseB).b;
+          //
+          vec4 glitchColorR = texture2D(imageTexture, vUv + uv1NoiseR + uv2NoiseR);
+          vec4 glitchColorG = texture2D(imageTexture, vUv + uv1NoiseG + uv2NoiseG);
+          vec4 glitchColorB = texture2D(imageTexture, vUv + uv1NoiseB + uv2NoiseB);
+          vec4 glitchColorA = texture2D(imageTexture, vUv + uv1NoiseA + uv2NoiseA);
 
-          vec4 outColor = vec4(flowerColor.xyz * flowerColor.a, linesColor.a + flowerColor.a);
-          outColor.rgb += linesColor.rgb * linesColor.a;
+          vec4 outColor = vec4(
+            glitchColorR.r,
+            glitchColorG.g,
+            glitchColorB.b,
+            glitchColorA.a
+          );
 
-          gl_FragColor = outColor;
+          gl_FragColor = sRGBToLinear(outColor);
         }
 
         `,
         //
-      })
-    );
+      });
+
+      return shader;
+    };
+    this.fsQuad0 = new FullScreenQuad(makeShader({ uniforms: this.uniforms0 }));
+    this.fsQuad0._mesh.position.z += -0.0001;
+    this.fScene.add(this.fsQuad0._mesh);
+
+    this.fsQuad1 = new FullScreenQuad(makeShader({ uniforms: this.uniforms1 }));
+    this.fsQuad1._mesh.position.z += 0.0;
+    this.fScene.add(this.fsQuad1._mesh);
+
+    this.fsQuad2 = new FullScreenQuad(makeShader({ uniforms: this.uniforms2 }));
+    this.fsQuad2._mesh.position.z += 0.0001;
+    this.fScene.add(this.fsQuad2._mesh);
+
+    this.rttCamera = new OrthographicCamera(-1, 1, 1, -1, 0, 1);
 
     this.onLoop(({ gl }) => {
       gl.outputEncoding = sRGBEncoding;
@@ -180,10 +245,12 @@ class VideoAPI {
       let mgl = gl;
 
       mgl.setRenderTarget(this.rttFBO);
-      this.fsQuad.render(mgl);
+      gl.render(this.fScene, this.rttCamera);
+      // this.fsQuad.render(mgl);
       mgl.setRenderTarget(null);
-      //
     });
+
+    this.rttFBO.texture.encoding = LinearEncoding;
 
     this.rttDisplayPlane = new Mesh(
       new PlaneGeometry((650 / 100) * 0.5, (780 / 100) * 0.5),
