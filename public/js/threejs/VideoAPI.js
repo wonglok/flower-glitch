f5fc6b56b6df0922d18fee18f1478966b2ce95bd
Wong Lok <wonglok@users.noreply.github.com>
2022-11-23 16:26:38 +0800

    save

--- public/js/threejs/VideoAPI.js
+++ public/js/threejs/VideoAPI.js
@@ -16,8 +16,9 @@ import {
   PlaneGeometry,
   DoubleSide,
   PerspectiveCamera,
+  BufferGeometry,
+  Float32BufferAttribute,
 } from "./build/three.module.js";
-import { FullScreenQuad } from "./examples/jsm/postprocessing/EffectComposer.js";
 
 class VideoAPI {
   constructor({ importObjects }) {
@@ -408,3 +409,38 @@ class VideoAPI {
 }
 
 export { VideoAPI };
+
+// Helper for passes that need to fill the viewport with a single quad.
+
+const _camera = new OrthographicCamera(-1, 1, 1, -1, 0, 1);
+
+// https://github.com/mrdoob/three.js/pull/21358
+
+const _geometry = new BufferGeometry();
+_geometry.setAttribute(
+  "position",
+  new Float32BufferAttribute([-1, 3, 0, -1, -1, 0, 3, -1, 0], 3)
+);
+_geometry.setAttribute("uv", new Float32BufferAttribute([0, 2, 0, 0, 2, 0], 2));
+
+class FullScreenQuad {
+  constructor(material) {
+    this._mesh = new Mesh(_geometry, material);
+  }
+
+  dispose() {
+    this._mesh.geometry.dispose();
+  }
+
+  render(renderer) {
+    renderer.render(this._mesh, _camera);
+  }
+
+  get material() {
+    return this._mesh.material;
+  }
+
+  set material(value) {
+    this._mesh.material = value;
+  }
+}
